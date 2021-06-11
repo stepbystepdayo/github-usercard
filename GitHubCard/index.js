14cebdc4da0277b064e152426e2fdfc0ca0044ab
Sayo <sl.deve080412@gmail.com>
2021-06-10 17:23:41 -0700

    finished most of MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -7,9 +7,9 @@ import axios from "axios"
 */
 
 
-const gitHubData = axios.get(`https://api.github.com/users/stepbystepdayo`)
-  .then((res)=> console.log(res))
-  .catch((err)=> console.log(err))
+// const gitHubData = axios.get(`https://api.github.com/users/stepbystepdayo`)
+  // .then((res)=> console.log(res))
+  // .catch((err)=> console.log(err))
   
 
 
@@ -26,6 +26,33 @@ const gitHubData = axios.get(`https://api.github.com/users/stepbystepdayo`)
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
+//get the data from github
+
+const cards = document.querySelector('.cards')
+// console.log(cards)
+
+const cardMaker = (user) =>{
+  return axios.get(`https://api.github.com/users/${user}`)
+  .then((res)=> {
+    // console.log(res.data)
+    const newCard = gitHubCardMaker(res.data)
+    cards.appendChild(newCard)
+  })
+   .catch('failed')
+  
+  }
+
+  cardMaker('stepbystepdayo')
+
+// const getDog = (breed)=>{
+//   return axios.get(`https://dog.ceo/api/breed/${breed}/images/random/{number}`)
+//   .then(({data})=>{
+//     const imageURL = data.message[0]
+//     const newCard = dogCardMaker({imageURL , breed})
+//     entryPoint.appendChild(newCard)
+//   })
+//   .catch(err => console.log(err))
+  
 
 
 
@@ -40,8 +67,10 @@ const gitHubData = axios.get(`https://api.github.com/users/stepbystepdayo`)
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
-
+const followersArray = ['tryingtokeepup', 'tetondan','dustinmyers','justsml', 'bigknell'];
+followersArray.forEach(follower =>{
+  cardMaker(follower)
+})
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -73,26 +102,49 @@ const followersArray = [];
 
 function gitHubCardMaker (obj) {
 
+  console.log('hey I got your gitHub data', obj)
+  console.log('here is my image, ', obj.avatar_url)
+
+  
   const githubCard = document.createElement('div')
   const image = document.createElement('img')
+  image.src =obj.avatar_url
+
   const info =document.createElement('div')
+  
   const name = document.createElement('h3')
-  const username = document.createElement('p')
+  console.log('this is my name,', obj.name)
+  name.textContent = obj.name
+
+  const usernameP = document.createElement('p')
+  usernameP.textContent = obj.login
   const location = document.createElement('p')
+  location.textContent = obj.location || 'fantasy Land!(^ U ^)'
   const portfolio = document.createElement('p')
+  portfolio.textContent = 'profile: '
   const portfolioA = document.createElement('a')
+  portfolioA.href  = obj.html_url;
+  portfolioA.textContent = obj.html_url;
+  portfolioA.target = '_blank';
   const followers = document.createElement('p')
+  followers.textContent = `followers : ${obj.followers}`;
   const following = document.createElement('p')
+  following.textContent = `following : ${obj.following}`;
   const bio = document.createElement('p')
+  if (obj.bio === null) {
+    bio.textContent = `Bio: I am ${obj.name} and an Engineer!`;
+  } else bio.textContent = `Bio: ${obj.bio}`;
+  
 
   githubCard.classList.add('card')
   info.classList.add('card-info')
   name.classList.add('name')
-  username.classList.add('username')
+  usernameP.classList.add('username')
 
   githubCard.appendChild(image)
   githubCard.appendChild(info)
-  info.appendChild(username)
+  info.appendChild(name)
+  info.appendChild(usernameP)
   info.appendChild(location)
   info.appendChild(portfolio)
   info.appendChild(followers)
@@ -100,11 +152,12 @@ function gitHubCardMaker (obj) {
   info.appendChild(bio)
   portfolio.appendChild(portfolioA)
 
-  image.src = obj.image
+  
 
 
 console.log(githubCard)
 
+return githubCard;
 }
 
 gitHubCardMaker({})
